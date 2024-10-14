dd5b1fdda1483856244609129dfe20cd169174cd
ruman321 <ruman.mail421@gmail.com>
2024-10-14 18:34:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IvcgxZAa#C7wAUz6FPdqhk5bCUZkDYm8ZfNw6EuLcjkDnpk3UWe4",
+SESSION_ID: process.env.SESSION_ID || "I79H2LrB#PuZ_hRowUYet_pkt7ftAEWvXgCVLZpChFxbOXoUrBU0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg",
 SUDO_NB: process.env.SUDO_NB || "94715922193",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
